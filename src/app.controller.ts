75e9dbd639f528ffc213432d58fcbacbf38de250
Thalyson Luiz <thalysonluiz@gmail.com>
2022-12-13 11:46:11 -0300

    nestjs and prisma

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { PrismaService } from './prisma.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
